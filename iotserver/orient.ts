f860885ccfb595166512d7806d97f28b04ccddb6
Sachin Patney <spatney@microsoft.com>
2016-03-20 22:09:08 -0700

    update

--- iotserver/orient.ts
+++ iotserver/orient.ts
@@ -28,7 +28,7 @@ class Glimpse implements IGlimpse {
 
         options.host.on('update',(data: any)=>{
             this.value = data;
-            this.circle.attr('r', viewport.height / 4 * data);
+            this.circle.attr('r', this.viewport.height / 4 * data);
         });
     }
 
